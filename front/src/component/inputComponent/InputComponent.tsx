fe2ca7505ba8e93b562c41030ccf55312b7ec3a4
Paweł Zarzycki <pawelzarzyckimail@gmail.com>
2020-11-14 00:32:05 +0100

    feat: created inputComponent and sceleton of login page

---
+++ front/src/component/inputComponent/InputComponent.tsx
@@ -0,0 +1,43 @@
+import React, { FC, useState } from 'react';
+import '../../scss/components/inputComponent.scss';
+import passwordIcon from '../../assets/passwordIcon.svg';
+
+interface InputComponentProps {
+    type: string,
+    labelText: string,
+    value: string,
+}
+
+export const InputComponent: FC<InputComponentProps> = ({ type, labelText, value }) => {
+    const [showPassword, setShowPassword] = useState(false);
+
+
+    const handleShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+    return (
+        <div className="inputWrapper">
+            <label>{labelText}</label>
+            <div className="inputWrapper__wrapperInputAndIcon">
+                <input
+                    className="inputWrapper__wrapperInputAndIcon--input"
+                    type={
+                        type === "password"
+                            ? showPassword
+                                ? "text"
+                                : "password"
+                            : type
+                    }
+                    value={value}
+                >
+                </input>
+                {type === "password" ?
+                    <img
+                        src={passwordIcon}
+                        onClick={handleShowPassword}
+                        alt="visibility password icon"
+                    /> : null}
+            </div>
+        </div >
+    )
+}
